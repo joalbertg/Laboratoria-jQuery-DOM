8b6047c13bafaf6842de382339772259d1cd01e5
Joalbert González <joalbertgonzalez@gmail.com>
2017-12-17 19:07:15 -0500

    window y document load and ready

--- js/app.js
+++ js/app.js
@@ -1,5 +1,34 @@
+// vanillaJS
+window.onload = function() {
+  console.log('Se ha cargado la ventana semánticamente!!!');
+}
+
+window.addEventListener('load', function() {
+  console.log('Se ha cargado la ventana con eventos múltiples!!!');
+})
+
+document.onready = function() {
+  console.log('Se ha cargado el documento semánticamente!!!');    
+}
+
+// no existe ya que solo ocurre una vez y no se puede prender y apagar
+/* document.addEventListener('ready', function() {
+  console.log('Se ha cargado el documento con eventos múltiples!!!');  
+}) */
+
+// jQuery
+jQuery(window).load(function() {
+  console.log('Se ha cargado la ventana semánticamente con jQuery!!!');  
+})
+
+jQuery(window).on('load', function() {
+  console.log('Se ha cargado la ventana con eventos múltiples con jQuery!!!');  
+})
+
 jQuery(document).ready(function() {
-  console.log(jQuery('#p-test').text());
-  console.log(jQuery('#p-test').html());
-  console.log(jQuery('#input-test').val());
-});
+  console.log('Se ha cargado el documento semánticamente con jQuery!!!');      
+})
+
+jQuery(document).on('ready', function() {
+  console.log('Se ha cargado el documento con eventos múltiples con jQuery!!!');      
+})
