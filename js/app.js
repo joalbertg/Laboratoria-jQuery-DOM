689d800d3dad65793f71f860feec6965268c6813
Joalbert González <joalbertgonzalez@gmail.com>
2017-12-18 16:25:19 -0500

    jQuery slideDown, slideUp, slideToggle

--- js/app.js
+++ js/app.js
@@ -1,33 +1,19 @@
 function begin() {
-  jQuery('#p-test').click(function() {
-    jQuery(this).hide();
-  })
-
-  jQuery('#boton-1').on('click', function() {
-    jQuery('#p-test').show();
-  })
-
-  $('#boton-2').click(function() {
-    $('#p-test').hide('fast');
-  })
-
-  $('#boton-3').click(function() {
-    $('#p-test').show('slow');
-  })
-
-  $('#boton-4').click(function() {
-    $('#p-test').toggle();
-  })
-
-  $('#boton-5').click(function() {
-    $('#p-test').toggle('swing');
-  })
-
-  // $('#p-test').css('background-color', 'yellow');
-  /* $('#p-test').css({
-    backgroundColor: 'yellow',
-    border: '1px solid black'
-  }); */
+  efectos();
 }
 
-window.onload = begin;
+function efectos() {
+  $('.mostrar').on('click', function() {
+    $('.deslizante').slideDown(1500);
+  })
+
+  $('.ocultar').on('click', function() {
+    $('.deslizante').slideUp('fast');
+  })
+
+  $('.mostrar-ocultar').on('click', function() {
+    $('.deslizante').slideToggle();
+  })
+}
+
+window.addEventListener('load', begin);
